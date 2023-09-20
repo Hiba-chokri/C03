0a8be9893e26b03fada4dd7cff4d5993d5b0a0ad
Hiba-chokri <111317168+Hiba-chokri@users.noreply.github.com>
2023-09-20 08:32:51 +0100

    Add files via upload
    c04

---
+++ ex05/ft_strlcat.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hichokri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 15:32:02 by hichokri          #+#    #+#             */
+/*   Updated: 2023/09/17 18:33:31 by hichokri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+unsigned	int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	len_dest;
+	unsigned int	len_src;
+
+	len_src = ft_strlen(src);
+	len_dest = ft_strlen(dest);
+	i = 0;
+	if (size == 0 || size <= len_dest)
+		return (len_src + size);
+	while (src[i] != '\0' && i < size - len_dest - 1)
+	{
+		dest[len_dest + i] = src[i];
+		i++;
+	}
+	dest[len_dest + i] = '\0';
+	return (len_src + len_dest);
+}
